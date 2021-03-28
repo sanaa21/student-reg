ee53852d055f969f1d5622fdc7e4ddd5ecc2b8da
sanaa aljundi <81189433+sanaa21@users.noreply.github.com>
2021-03-28 15:07:55 +0300

    Update window.js

--- window.js
+++ window.js
@@ -1,1 +1,18 @@
 
+var userName = prompt('What is your name ?');
+var age = ptompt('How old are you ?');
+if ( age >= 18){
+  
+var serv = prompt('what is your serv!','Home&login,sign up,About us');
+if(serv === 'Home&login'){
+    document.write('<a href='#'/>')
+}else if (serv=== 'sign up'){
+    document.write('<a href="Q2.html"/>')
+}
+else if (serv === 'About us'){
+    document.write('<a href="Q3.html"/>')
+    }
+    else {
+    alert('Welcome to our Acadimy')
+}
+}
