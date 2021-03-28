1ece0f3cfc3685c309bffbfecd0088830e815937
sanaa aljundi <81189433+sanaa21@users.noreply.github.com>
2021-03-28 16:55:13 +0300

    Update window.js

--- window.js
+++ window.js
@@ -1,9 +1,10 @@
 var userName = prompt('What is your name ?');
+var pic2 = prompt( 'which pic ?' , 'school , student');
 var pic = prompt('How many school pic , student pic do u like to see ?');
-var pic2= prompt( 'which pic ?' , 'school , student');
-while(pic2!=='school'&& pic2!=='student'){
-    pic=ptompt('plz enter on of them student or school..pic');
-}
+var pic2 = prompt( 'which pic ?' , 'school , student');
+//while(pic2!=='school'&& pic2!=='student'){
+   // pic=ptompt('plz enter on of them student or school..pic');
+//}
   for (var i = 1 ; i <=pic; i++ ){
 
 if(pic === 'school'){
@@ -11,8 +12,9 @@ if(pic === 'school'){
 }else if (pic=== 'student'){
     document.write('<img src= "https://m.economictimes.com/thumb/msid-77391367,width-1200,height-900,resizemode-4,imgsize-677864/to-reduce-risk-experts-say-schools-should-make-adjustments-when-resuming-in-person-classes-.jpg "  width="100" height="100"/>')
 }
-    else {
-    alert('Welcome to our Acadimy')
-}
+    
 
+else { alert('Welcome to our Acadimy')
+}
+    
   }
