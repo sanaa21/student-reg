17932b581b83ed68410223efe1b9a0ab320c6672
sanaa aljundi <81189433+sanaa21@users.noreply.github.com>
2021-03-28 16:58:56 +0300

    Update window.js

--- window.js
+++ window.js
@@ -1,15 +1,16 @@
+
 var userName = prompt('What is your name ?');
 var pic2 = prompt( 'which pic ?' , 'school , student');
 var pic = prompt('How many school pic , student pic do u like to see ?');
-var pic2 = prompt( 'which pic ?' , 'school , student');
+
 //while(pic2!=='school'&& pic2!=='student'){
    // pic=ptompt('plz enter on of them student or school..pic');
 //}
-  for (var i = 1 ; i <=pic; i++ ){
+  for (var i = 1 ; i <=pic2; i++ ){
 
-if(pic === 'school'){
+if(pic2 === 'school'){
     document.write('<img src= " https://www.greatschools.org/gk/wp-content/uploads/2014/03/The-school-visit-what-to-look-for-what-to-ask-1-360x180.jpg"/>') 
-}else if (pic=== 'student'){
+}else if (pic2=== 'student'){
     document.write('<img src= "https://m.economictimes.com/thumb/msid-77391367,width-1200,height-900,resizemode-4,imgsize-677864/to-reduce-risk-experts-say-schools-should-make-adjustments-when-resuming-in-person-classes-.jpg "  width="100" height="100"/>')
 }
     
